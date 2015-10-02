13f4d85139aa47cab0fb89b329fe0a7dfc06ab9a
Anastasia <an.klyonskaya@gmail.com>
2015-10-02 15:16:03 +0300

    .

---
+++ RestTest/RestTest/OrderResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace RestTest
+{
+    class OrderResponse
+    {
+        public Link request;
+        public Link who;
+        public Link[] fulfillment;
+    }
+}
