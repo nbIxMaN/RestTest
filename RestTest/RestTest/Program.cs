ed3ea972a51c5d1c26f84d4a11b22f8e0729d80e
nbIxMaN <Shulllik@gmail.com>
2015-10-05 17:16:14 +0300

    +

--- RestTest/RestTest/Program.cs
+++ RestTest/RestTest/Program.cs
@@ -4,9 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
+using Newtonsoft.Json;
+using RestSharp.Serializers;
+using System.IO;
 
 namespace RestTest
 {
+        /// <summary>
+        /// Default JSON serializer for request bodies
+        /// Doesn't currently use the SerializeAs attribute, defers to Newtonsoft's attributes
+        /// </summary>
+    public class RestSharpJsonNetSerializer : ISerializer
+    {
+        private readonly Newtonsoft.Json.JsonSerializer _serializer;
+
+        /// <summary>
+        /// Default serializer
+        /// </summary>
+        public RestSharpJsonNetSerializer()
+        {
+            ContentType = "application/json";
+            _serializer = new Newtonsoft.Json.JsonSerializer
+            {
+                MissingMemberHandling = MissingMemberHandling.Ignore,
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore
+            };
+        }
+
+        /// <summary>
+        /// Default serializer with overload for allowing custom Json.NET settings
+        /// </summary>
+        public RestSharpJsonNetSerializer(Newtonsoft.Json.JsonSerializer serializer)
+        {
+            ContentType = "application/json";
+            _serializer = serializer;
+        }
+
+        /// <summary>
+        /// Serialize the object as JSON
+        /// </summary>
+        /// <param name="obj">Object to serialize
+        /// <returns>JSON as String</returns>
+        public string Serialize(object obj)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var jsonTextWriter = new JsonTextWriter(stringWriter))
+                {
+                    jsonTextWriter.Formatting = Formatting.Indented;
+                    jsonTextWriter.QuoteChar = '"';
+
+                    _serializer.Serialize(jsonTextWriter, obj);
+
+                    var result = stringWriter.ToString();
+                    return result;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Unused for JSON Serialization
+        /// </summary>
+        public string DateFormat { get; set; }
+        /// <summary>
+        /// Unused for JSON Serialization
+        /// </summary>
+        public string RootElement { get; set; }
+        /// <summary>
+        /// Unused for JSON Serialization
+        /// </summary>
+        public string Namespace { get; set; }
+        /// <summary>
+        /// Content type for serialized content
+        /// </summary>
+        public string ContentType { get; set; }
+    }
+
+
     //class URLandCODEABLE : Extension
     //{
     //    public string url;
@@ -66,9 +141,13 @@ namespace RestTest
             //    }
             //};
             Patient pa = (new SetData()).SetPatient();
+            pa.address = null;
+            pa.name = null;
+            pa.resourceType = null;
             var c = new RestClient();
             c.BaseUrl = new Uri("http://fhir.zdrav.netrika.ru/fhir/Patient");
             var request = new RestRequest(Method.POST);
+            request.JsonSerializer = new RestSharpJsonNetSerializer();
             var s = request.JsonSerializer.Serialize(pa);
             request.AddHeader("Authorization", "N3 f0a258e5-92e4-47d3-9b6c-89362357b2b3");
             request.RequestFormat = DataFormat.Json;
