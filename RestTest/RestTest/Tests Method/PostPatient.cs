385dfa6c2de807d46d2d1930924af70791674c46
Anastasia Klyonskaya <an.klyonskaya@gmail.com>
2015-10-08 19:25:50 +0300

    0

--- RestTest/RestTest/Tests Method/PostPatient.cs
+++ RestTest/RestTest/Tests Method/PostPatient.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using RestSharp;
+using Newtonsoft.Json;
+using RestSharp.Serializers;
+using System.IO;
 
 namespace RestTest.Tests_Method
 {
@@ -13,7 +17,16 @@ namespace RestTest.Tests_Method
         [Test]
         public void Test()
         {
+            Patient p = (new SetData()).SetPatient();
+            var c = new RestClient();
+            c.BaseUrl = new Uri("http://fhir.zdrav.netrika.ru/fhir/Patient");
+            var request = new RestRequest(Method.POST);
 
+            var s = request.JsonSerializer.Serialize(p);
+            request.AddHeader("Authorization", "N3 f0a258e5-92e4-47d3-9b6c-89362357b2b3");
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(p);
+            var r = c.Execute(request);
         }
     }
 }
