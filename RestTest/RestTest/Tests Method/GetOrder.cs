7f982d4caf6ed422c8f38cffc69b3eef1972ed5e
Anastasia <an.klyonskaya@gmail.com>
2015-10-16 15:10:18 +0300

    +

---
+++ RestTest/RestTest/Tests Method/GetOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RestTest.Tests_Method
+{
+    [TestFixture]
+    class GetOrder
+    {
+        [Test]
+        public void Test()
+        {
+            GetRequest g = new GetRequest
+            {
+                parameter = new Parameter[]
+                {
+                    new Parameter
+                    {
+                        name = "TargetCode",
+                        valueString = "123.456.789"
+                    },
+                    new Parameter
+                    {
+                        name = "Barcode",
+                        valueString ="barcode"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ RestTest/RestTest/Tests Method/PostPatient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serializers;
+using System.IO;
+using Hl7.Fhir.Model;
+
+namespace RestTest.Tests_Method
+{
+    [TestFixture]
+    class PostPatient
+    {
+        [Test]
+        public void Test()
+        {
+            Patient p = (new SetData()).SetPatient();
+            var c = new RestClient();
+            c.BaseUrl = new Uri("http://fhir.zdrav.netrika.ru/fhir/Patient");
+            var request = new RestRequest(Method.POST);
+
+            var s = request.JsonSerializer.Serialize(p);
+            request.AddHeader("Authorization", "N3 f0a258e5-92e4-47d3-9b6c-89362357b2b3");
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(p);
+            var r = c.Execute(request);
+        }
+    }
+}
