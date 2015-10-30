cdd3c869a71c9da8b25a08035a11dec0dfc51e3c
nbIxMaN <Shulllik@gmail.com>
2015-10-30 15:50:27 +0300

    =

--- RestTest/RestTest/Tests Method/GetStatus.cs
+++ RestTest/RestTest/Tests Method/GetStatus.cs
@@ -21,23 +21,24 @@ namespace RestTest.Tests_Method
             //задаём ресурсы
             Order order = (new SetData()).SetOrder(patient, pract, References.organization);
             DiagnosticOrder diagnosticOrder = (new SetData()).SetDiagnosticOrder(patient, pract, References.encounter,
-                                                                 null, null);
-            //задаём Bundle
+                                                                null, null);
+
+            //задаём Bundle 
             Bundle b = (new SetData()).SetBundleOrder(order, diagnosticOrder, null, null, null, null, null, null, null);
 
             string s = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(b);
             IRestResponse resp = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir?_format=json", s);
-            Bundle p = (Bundle)Hl7.Fhir.Serialization.FhirParser.ParseResourceFromJson(resp.Content);
-            string id = p.Entry[0].Resource.Id;
-            string source = order.Identifier[0].Assigner.Reference;
-            source = source.Substring(source.IndexOf('/') + 1, source.Length - source.IndexOf('/') - 1);
-            string orderId = id;
-            string orderMis = order.Identifier[0].Value;
+            //Bundle p = (Bundle)Hl7.Fhir.Serialization.FhirParser.ParseResourceFromJson(resp.Content);
+            //string id = p.Entry[0].Resource.Id;
+            //string source = orderResp.Identifier[0].Assigner.Reference;
+            //source = source.Substring(source.IndexOf('/') + 1, source.Length - source.IndexOf('/') - 1);
+            //string orderId = id;
+            //string orderMis = orderResp.Identifier[0].Value;
             Parameters a = new Parameters();
 
             //a.Add("SourceCode", new FhirString(source));
             //a.Add("OrderMisID", new FhirString(orderMis));
-            a.Add("OrderId", new FhirString(orderId));
+            a.Add("OrderId", new FhirString("99e4270e-a713-4fab-8ecf-e19161ece69c"));
             string s2 = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(a);
             IRestResponse resp2 = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir/$getstatus", s2);
             NUnit.Framework.Assert.Fail(resp2.Content);
@@ -67,8 +68,8 @@ namespace RestTest.Tests_Method
             string orderMis = order.Identifier[0].Value;
             Parameters a = new Parameters();
 
-            a.Add("SourceCode", new FhirString(source));
-            a.Add("OrderMisID", new FhirString(orderMis));
+            a.Add("SourceCode", new FhirString("4a94e705-ee3e-46fc-bba0-0298e0fd5bd2"));
+            a.Add("OrderMisID", new FhirString("IdOrderMis2"));
             //a.Add("OrderId", new FhirString(orderId));
             string s2 = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(a);
             IRestResponse resp2 = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir/$getstatus", s2);
