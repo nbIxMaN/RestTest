7f7a703a0f463c6dd3ba977f3db39a5f60023b13
nbIxMaN <Shulllik@gmail.com>
2015-11-02 19:02:03 +0300

    +

--- RestTest/RestTest/Tests Method/GetStatus.cs
+++ RestTest/RestTest/Tests Method/GetStatus.cs
@@ -47,37 +47,37 @@ namespace RestTest.Tests_Method
                 NUnit.Framework.Assert.Fail(resp2.Content);
         }
 
-        [Test]
-        public void GetStatusWithSourceCode()
-        {
-            //задаём ссылки
-            string patient = References.patient;
-            string pract = References.practitioner;
+        //[Test]
+        //public void GetStatusWithSourceCode()
+        //{
+        //    //задаём ссылки
+        //    string patient = References.patient;
+        //    string pract = References.practitioner;
 
-            //задаём ресурсы
-            Order order = (new SetData()).SetOrder(patient, pract, References.organization);
-            DiagnosticOrder diagnosticOrder = (new SetData()).SetDiagnosticOrder_Min(patient, pract, References.encounter,
-                                                                 null, null);
-            //задаём Bundle
-            Bundle b = (new SetData()).SetBundleOrder(order, diagnosticOrder, null, null, null, null, null, null, null);
+        //    //задаём ресурсы
+        //    Order order = (new SetData()).SetOrder(patient, pract, References.organization);
+        //    DiagnosticOrder diagnosticOrder = (new SetData()).SetDiagnosticOrder_Min(patient, pract, References.encounter,
+        //                                                         null, null);
+        //    //задаём Bundle
+        //    Bundle b = (new SetData()).SetBundleOrder(order, diagnosticOrder, null, null, null, null, null, null, null);
 
-            string s = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(b);
-            IRestResponse resp = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir?_format=json", s);
-            Bundle p = (Bundle)Hl7.Fhir.Serialization.FhirParser.ParseResourceFromJson(resp.Content);
-            string id = p.Entry[0].Resource.Id;
-            string source = order.Identifier[0].Assigner.Reference;
-            source = source.Substring(source.IndexOf('/') + 1, source.Length - source.IndexOf('/') - 1);
-            string orderId = id;
-            string orderMis = order.Identifier[0].Value;
-            Parameters a = new Parameters();
+        //    string s = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(b);
+        //    IRestResponse resp = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir?_format=json", s);
+        //    Bundle p = (Bundle)Hl7.Fhir.Serialization.FhirParser.ParseResourceFromJson(resp.Content);
+        //    string id = p.Entry[0].Resource.Id;
+        //    string source = order.Identifier[0].Assigner.Reference;
+        //    source = source.Substring(source.IndexOf('/') + 1, source.Length - source.IndexOf('/') - 1);
+        //    string orderId = id;
+        //    string orderMis = order.Identifier[0].Value;
+        //    Parameters a = new Parameters();
 
-            a.Add("SourceCode", new FhirString(source));
-            a.Add("OrderMisID", new FhirString(orderMis));
-            //a.Add("OrderId", new FhirString(orderId));
-            string s2 = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(a);
-            IRestResponse resp2 = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir/$getstatus", s2);
-            NUnit.Framework.Assert.Fail(resp2.Content);
-        }
+        //    a.Add("SourceCode", new FhirString(source));
+        //    a.Add("OrderMisID", new FhirString(orderMis));
+        //    //a.Add("OrderId", new FhirString(orderId));
+        //    string s2 = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(a);
+        //    IRestResponse resp2 = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir/$getstatus", s2);
+        //    NUnit.Framework.Assert.Fail(resp2.Content);
+        //}
         //[Test]
         //public void GetReceivedStatus()
         //{
