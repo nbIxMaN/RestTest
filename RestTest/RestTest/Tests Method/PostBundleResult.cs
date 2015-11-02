c6f2882c05a2a41910472ee8eb0b3b69e0ce424f
Anastasia <an.klyonskaya@gmail.com>
2015-11-02 16:45:43 +0300

    q

--- RestTest/RestTest/Tests Method/PostBundleResult.cs
+++ RestTest/RestTest/Tests Method/PostBundleResult.cs
@@ -126,5 +126,45 @@ namespace RestTest.Tests_Method
             //string bundleAnsw = Newtonsoft.Json.JsonConvert.DeserializeObject(resp.Content).ToString();
             Assert.Fail(resp.Content);
         }
+        
+        //-----------------------------------Put
+
+         /// <summary>
+        /// OrderResponse, DiagnosticReport, Observation, Practitioner
+        /// </summary>
+        [Test]
+        public void BundleResult_PutPractitioner()
+        {
+            //задаём ссылки
+            string patient = References.patient;
+            string pract = "ab1af9a5-91b0-4c7f-aba7-6eb4b8f43aab";
+
+            //задаём ресурсы
+            OrderResponse orderResp = (new SetData()).SetOrderResponse(patient, References.organization);
+            DiagnosticReport diagRep = (new SetData()).SetDiagnosticReport(patient, pract, References.diagnosticOrder);
+            Observation observ = (new SetData()).SetObservation_BundleResult_Reason(pract);
+            Practitioner practitioner = (new SetData()).SetPractitioner();
+            practitioner.Id = pract;
+            practitioner.Name.Family = new List<string> { "FamilyName" + DateTime.Now };
+            practitioner.Meta = new Meta
+            {
+                //постоянно меняется, пока что это поле заполняется, выяснением этого значения вручную
+                VersionId = "b97ff454-4cf1-4855-90d5-d7adb058f319"
+            };
+
+            //задаём Bundle 
+            Bundle bRes = (new SetData()).SetBundleResult(orderResp, diagRep, observ, null);
+            Bundle.BundleEntryComponent component = new Bundle.BundleEntryComponent
+            {
+                Resource = practitioner,
+                Transaction = new Bundle.BundleEntryTransactionComponent() { Method = Bundle.HTTPVerb.PUT, Url = References.practitioner }
+            };
+            bRes.Entry.Add(component);
+
+            string s = Hl7.Fhir.Serialization.FhirSerializer.SerializeResourceToJson(bRes);
+            IRestResponse resp = (new Program()).RequestExec(Method.POST, "http://192.168.8.93:2223/fhir?_format=json", s);
+            //string bundleAnsw = Newtonsoft.Json.JsonConvert.DeserializeObject(resp.Content).ToString();
+            Assert.Fail(resp.Content);
+        }
     }
 }
